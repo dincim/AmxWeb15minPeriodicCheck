14cfdeb34477f2b034d6efa747adb6f98ceed8f8
Murat Dinc <mdinc@corp.amaxinsurance.com>
2021-08-19 12:23:32 -0500

    saucelab implemented

--- src/test/java/amaxwebsite15mincheck/AmaxWebsiteTest15.java
+++ src/test/java/amaxwebsite15mincheck/AmaxWebsiteTest15.java
@@ -12,28 +12,31 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class AmaxWebsiteTest15 {
+public class AmaxWebsiteTest15 extends BaseTest {
 
-	WebDriver driver;
-
-	@BeforeMethod
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", 
-				"C:\\browserdrivers\\chromedriver.exe");
-
-		driver = new ChromeDriver();
-
-		driver.get("https://www.amaxinsurance.com/");
-		
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	}
+//	WebDriver driver;
+//
+//	@BeforeMethod
+//	public void setUp() {
+//		System.setProperty("webdriver.chrome.driver", 
+//				"C:\\browserdrivers\\chromedriver.exe");
+//
+//		driver = new ChromeDriver();
+//
+//		driver.get("https://www.amaxinsurance.com/");
+//		
+//		driver.manage().window().maximize();
+//		driver.manage().deleteAllCookies();
+//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//	}
+	
+	
 	
 	@Test(priority=1)
 
 	public void amaxQuoteEnginePageTitleTest() throws InterruptedException {
 
+		driver.get("https://www.amaxinsurance.com/");
 		
 		driver.findElement(By.xpath("//input[@class='cmp-form-text__text']")).sendKeys("75247");
 		
@@ -54,14 +57,15 @@ public class AmaxWebsiteTest15 {
 	@Test(priority=2)
 	public void amaxWebSiteHomePageTitleTest() {
 		
+		driver.get("https://www.amaxinsurance.com/");
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "A-MAX Auto Insurance | Affordable Car Insurance";
 		Assert.assertEquals(actualTitle, expectedTitle, "HomePage title does not match");
 		
 	}
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
-	}
+//	@AfterMethod
+//	public void tearDown() {
+//		driver.quit();
+//	}
 
 }
